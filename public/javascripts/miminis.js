abd85afc5ab0ec769d6d7215f13ebda7582064f6
Eric <eric@TheColony.(none)>
2013-11-03 01:02:39 -0400

    Added in support for instagram

--- public/javascripts/miminis.js
+++ public/javascripts/miminis.js
@@ -1,109 +1,183 @@
 var map = null;
 var last = 300000;
 var mdata = 1;
-var miminis_windows = new Array(); 
+var miminis_windows = new Array();
+var miminis_windows2 = new Array();
 
 function sendTweet() {
-	var tweet = $(Tweet).val();
-	navigator.geolocation.getCurrentPosition(function (position) {
-		var data = {tweet: tweet, latitude: position.coords.latitude, longitude: position.coords.longitude};
-		$.ajax({
-			url: "sendTweet",
-			type: "POST",
-			data: data,
-			success: getTweets,
-			dataType: "text"});
-		
-		
-	}, mapError, { timeout: 10000,
-         enableHighAccuracy: true,
-         maximumAge:0 });
+    var tweet = $(Tweet).val("");
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var data = {
+            tweet: tweet,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+        $.ajax({
+            url: "sendTweet",
+            type: "POST",
+            data: data,
+            success: getTweets,
+            dataType: "text"
+        });
+
+
+    }, mapError, {
+        timeout: 10000,
+        enableHighAccuracy: true,
+        maximumAge: 0
+    });
 
 }
 
 function initMap(position) {
-	var mapOptions = {
-		zoom: 15,
-		center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	  };
-	  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 function mapError() {
-	alert("Error");
+    alert("Error");
 }
 
 function init() {
-	navigator.geolocation.getCurrentPosition(initMap, mapError, { timeout: 10000,
-         enableHighAccuracy: true,
-         maximumAge:0 });
+    navigator.geolocation.getCurrentPosition(initMap, mapError, {
+        timeout: 10000,
+        enableHighAccuracy: true,
+        maximumAge: 0
+    });
+}
+
+function getInstagram() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var urlx = encodeURI("searchInstagram?q=miminis&lat=" + position.coords.latitude + "&long=" + position.coords.longitude);
+        $.ajax({
+            url: urlx,
+            type: "GET",
+            success: function(data) {
+                parseData(data, instagramHandler, miminis_windows2);
+            },
+            contentType: "charset=utf-8",
+            dataType: "json",
+            timeout: 20000
+        })
+    }, mapError, null);
+}
+
+
+function googleHandler(data, oms, miminis_window) {
+    data = data.statuses 
+    $.each(data, function(i, item) {
+        if (item.geo) {
+            var position = new google.maps.LatLng(item.geo.coordinates[0], item.geo.coordinates[1]);
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: item.user.name
+            });
+            var contentString = generateString(item);
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            miminis_window[i] = infowindow;
+            google.maps.event.addListener(marker, 'click', function() {
+                for (var p = 0; p < miminis_windows2.length; p++) {
+                    if (miminis_windows2[p]) miminis_windows2[p].close();
+                }
+                for (var p = 0; p < miminis_window.length; p++) {
+                    if (miminis_window[p]) miminis_window[p].close();
+                }
+                if (map.getStreetView().getVisible()) {
+                    infowindow.open(map.getStreetView(), this);
+                } else {
+                    infowindow.open(map, this);
+                }
+            });
+            oms.addMarker(marker);
+        }
+    });
+}
+function instagramHandler(data, oms, miminis_window) {
+    $.each(data, function(i, item) {
+        if (item) {
+            var position = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: item.location.name
+            });
+	    iconFile = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'; 
+	marker.setIcon(iconFile) 
+            var contentString = generateStringInstagram(item);
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString,
+            });
+            miminis_window[i] = infowindow;
+            google.maps.event.addListener(marker, 'click', function() {
+                for (var p = 0; p < miminis_windows.length; p++) {
+                    if (miminis_windows[p]) miminis_windows[p].close();
+                }
+                for (var p = 0; p < miminis_window.length; p++) {
+                    if (miminis_window[p]) miminis_window[p].close();
+                }
+                if (map.getStreetView().getVisible()) {
+                    infowindow.open(map.getStreetView(), this);
+                } else {
+                    infowindow.open(map, this);
+                }
+            });
+            oms.addMarker(marker);
+        }
+    });
+}
+
+function parseData(data, mapHandler, windowHandler) {
+    mdata = data;
+    console.log('fineinshed');
+    var oms = new OverlappingMarkerSpiderfier(map);
+    var gm = google.maps;
+    var iw = new gm.InfoWindow();
+    oms.addListener('click', function(marker, event) {
+        if (!map.getStreetView().getVisible()) {
+            iw.setContent(marker.desc);
+            iw.open(map, marker);
+        }
+    });
+    oms.addListener('spiderfy', function(markers) {
+        if (!map.getStreetView().getVisible()) {
+            iw.close();
+        }
+    });
+    google.maps.event.addListener(map.getStreetView(), 'pano_changed', function() {});
+    mapHandler(data, oms, windowHandler); 
 }
 
 function getTweets() {
-	console.log('start');
-	navigator.geolocation.getCurrentPosition(function (position) {   
-		var urlx = encodeURI("search?q=miminis&lat="+position.coords.latitude+"&long="+position.coords.longitude);
-		console.log("Boo");
-		$.ajax({
-			url: urlx,
-			type: "GET",
-			success: parseData,
-			contentType: "charset=utf-8",
-			dataType: "json",
-			timeout: 20000
-			})
-	},mapError,null );
+    var tweet = $(Tweet).val();
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var urlx = encodeURI("search?q=miminis&lat=" + position.coords.latitude + "&long=" + position.coords.longitude);
+        $.ajax({
+            url: urlx,
+            type: "GET",
+            success: function(data) {
+                parseData(data, googleHandler, miminis_windows);
+            },
+            contentType: "charset=utf-8",
+            dataType: "json",
+            timeout: 20000
+        })
+    }, mapError, null);
 }
 
-function parseData(data) {
-	mdata = data;
-	console.log('fineinshed');
-	var oms = new OverlappingMarkerSpiderfier(map);
-	var gm = google.maps;
-	var iw = new gm.InfoWindow();
-	oms.addListener('click', function(marker, event) {
-		if(!map.getStreetView().getVisible()) {
-			iw.setContent(marker.desc);
-			iw.open(map, marker);
-		}
-	});
-	oms.addListener('spiderfy', function(markers) {
-		if(!map.getStreetView().getVisible()) {
-			iw.close();
-		}
-	});
-
-
-        google.maps.event.addListener(map.getStreetView(), 'pano_changed', function() {
-	  });
-	$.each(data.statuses, function(i, item) {		
-		if(item.geo) {
-			var position = new google.maps.LatLng(item.geo.coordinates[0], item.geo.coordinates[1]);
-			var marker = new google.maps.Marker({position:position, map:map, title: item.user.name}); 
-			var contentString = generateString(item); 
-			var infowindow = new google.maps.InfoWindow({
-				content: contentString
-			});
-			miminis_windows[i] = infowindow;
-			google.maps.event.addListener(marker, 'click', function() {
-				for(var p = 0; p < miminis_windows.length; p++) {
-					if(miminis_windows[p]) miminis_windows[p].close();
-				}
-				if(map.getStreetView().getVisible()) {
-					infowindow.open(map.getStreetView(), this); 
-				}
-				else {
-					infowindow.open(map, this); 
-				}
-			});
-			oms.addMarker(marker);
-		}
-	});
-	
-}
 function generateString(item) {
-	//var str = '<img src=\'' + item.user.entities.profile.background + '\'' + '>'
-	return('<table><tr><td><img src=\''+item.user.profile_image_url+'\'></td><td style=\'max-width:140px; word-wrap:break-word;\'>' + item.text + "<br><a href = \"" + item.url + "\" target=\"_blank\">Go to Tweet</a>" +'</td></tr></table>');
+    //var str = '<img src=\'' + item.user.entities.profile.background + '\'' + '>'
+    return ('<table><tr><td><img src=\'' + item.user.profile_image_url + '\'></td><td style=\'max-width:140px; word-wrap:break-word;\'>' + item.text + "<br><a href = \"" + item.url + "\" target=\"_blank\">Go to Tweet</a>" + '</td></tr></table>');
+}
+function generateStringInstagram(item) {
+    //var str = '<img src=\'' + item.user.entities.profile.background + '\'' + '>'
+    return ('<table><tr><td><a href=\''+item.url+'\' target=\"_blank\"><img src=\'' + item.img_url.url + '\'></a></td></tr><tr><td style=\'max-width:140px; word-wrap:break-word;\'>'+item.caption+'</td></tr></table>');
 }
 getTweets();
+getInstagram();
--- routes/points.js
+++ routes/points.js
@@ -3,7 +3,6 @@ var https = require('https');
 var OAuth = require('oauth');
 var util = require('util');
 var Worker = require('webworker-threads').Worker;
-var async = require('async');
 var forEach = require('async-foreach').forEach;
 var oauth = new OAuth.OAuth(
     'https://api.twitter.com/oauth/request_token',
@@ -16,119 +15,133 @@ var oauth = new OAuth.OAuth(
 
 var url = require('url');
 
-function grabInstagram(obj, token) {
-    var buffer = [];
-    var result = [];
-    async.forEach(obj, function(item) {
+function grabInstagram(obj, token, res) {
+    var retResult = [];
+    var returned = {};
+    var needed = obj.length;
+    var gotten = 0;
+    for (var i = 0; i < obj.length; i++) {
+        https.get("https://api.instagram.com/v1/locations/" + obj[i].id + "/media/recent?access_token=" + token, function(result) {
+		result.setEncoding('utf8');
+		var buffer = [];
+		result.on('data', function(chunk) {
+			buffer.push(chunk);
+		});
+		result.on('end', function () {
+		    var text = buffer.join('');
+		    var obj = JSON.parse(text);
+		    try{
+		     	retResult[gotten] = {location: obj.data[0].location, 
+			img_url: obj.data[0].images.thumbnail,
+			url: obj.data[0].link,
+			caption: obj.data[0].caption.text};
+		    }
+		    catch(ee) {
+			retResult[gotten] = null;		
+		    }
+		    gotten++;
+		    if(gotten == needed) {
+			res.send(retResult);
+		    }
+		
+		});
+		
+	});
+    }
+}
 
-            var obj2 = https.get("https://api.instagram.com/v1/locations/" + id + "/media/recent?access_token=" + token, function(result) {
-                result.setEncoding('utf8');
-                result.on('data', function(chunk) {
-                    buffer.push(chunk);
-                });
-                result.on('end', function() {
-                    var text = buffer.join('');
-                    var obj = JSON.parse(text);
-                });
+exports.searchInstagram = function(req, res) {
+    var queryData = url.parse(req.url, true).query;
+    var buffer = [];
+
+    console.log("https://api.instagram.com/v1/locations/search?lat=" + queryData.lat + "&lng=" + queryData["long"] + "&access_token=" + req.cookies.instagram);
+    https.get("https://api.instagram.com/v1/locations/search?lat=" + queryData.lat + "&lng=" + queryData["long"] + "&access_token=" + req.cookies.instagram, function(result) {
+        result.setEncoding('utf8');
+        result.on('data', function(chunk) {
+            buffer.push(chunk);
+        });
+        result.on('end', function() {
+            var text = buffer.join('');
+            var obj = JSON.parse(text);
+            grabInstagram(obj.data, req.cookies.instagram, res);
+        });
+    });
+}
+
+
+exports.search = function(req, res) {
+    var queryData = url.parse(req.url, true).query;
+    //res.send(queryData);
+
+    oauth.get('https://api.twitter.com/1.1/search/tweets.json?count=100&geocode=' + queryData.lat + ',' + queryData["long"] + ',1mi',
+        config.keys['accessToken'],
+        config.keys['accessSecret'],
+        function(e, data, resp) {
+            var ret = new Array();
+            var obj = JSON.parse(data).statuses;
+            var process = new Worker(function() {
+                function process(obj) {
+                    var ret = new Array();
+                    var counter = 0;
+                    for (var i = 0; i < obj.length; i++) {
+                        var item = obj[i];
+                        if (item.geo) {
+                            ret[counter] = {
+                                geo: {
+                                    coordinates: item.geo.coordinates
+                                },
+                                text: item.text,
+                                user: {
+                                    profile_image_url: item.user.profile_image_url
+                                },
+                                url: "https://twitter.com/" + item.user.screen_name + "/status/" + item.id_str
+                            };
+                            counter++;
+                        }
+                    }
+                    return (ret);
+                }
+                onmessage = function(event) {
+                    postMessage(process(event.data));
+                }
             });
+            process.onmessage = function(event) {
+                var dx = {
+                    statuses: event.data
+                };
+                res.send(dx);
+            };
+            process.postMessage(obj);
+            //res.send(data);
+        });
+};
+
+exports.tweet = function(req, res) {
+    var idx = req.cookies.idx;
+    var token = req.cookies.token;
+    var secret = req.cookies.tokenSecret;
+    console.log(req.body);
+    console.log("Ewee");
+    oauth.post("https://api.twitter.com/1.1/statuses/update.json", token, secret, {
+            status: req.body.tweet,
+            "lat": req.body.latitude,
+            "long": req.body.longitude,
+            display_coordinates: true
+        },
+        function() {
+            res.send("Hello world");
         });
 }
 
-        exports.searchInstagram = function(req, res) {
-            var queryData = url.parse(req.url, true).query;
-            var buffer = [];
 
-            https.get("https://api.instagram.com/v1/locations/search?lat=" + queryData.lat + "&lng=" + queryData["long"] + "&access_token=" + req.cookies.instagram, function(result) {
-                result.setEncoding('utf8');
-                result.on('data', function(chunk) {
-                    buffer.push(chunk);
-                });
-                result.on('end', function() {
-                    var text = buffer.join('');
-                    var obj = JSON.parse(text);
-                    grabInstagram(obj, token);
-                    try {} catch (e) {
-                        res.send("Error");
-                    }
+exports.search2 = function(req, res) {
+    var queryData = url.parse(req.url, true).query;
+    //res.send(queryData);
 
-                });
-            });
-        }
-
-
-        exports.search = function(req, res) {
-            var queryData = url.parse(req.url, true).query;
-            //res.send(queryData);
-
-            oauth.get('https://api.twitter.com/1.1/search/tweets.json?count=100&geocode=' + queryData.lat + ',' + queryData["long"] + ',1mi',
-                config.keys['accessToken'],
-                config.keys['accessSecret'],
-                function(e, data, resp) {
-                    var ret = new Array();
-                    var obj = JSON.parse(data).statuses;
-                    var process = new Worker(function() {
-                        function process(obj) {
-                            var ret = new Array();
-                            var counter = 0;
-                            for (var i = 0; i < obj.length; i++) {
-                                var item = obj[i];
-                                if (item.geo) {
-                                    ret[counter] = {
-                                        geo: {
-                                            coordinates: item.geo.coordinates
-                                        },
-                                        text: item.text,
-                                        user: {
-                                            profile_image_url: item.user.profile_image_url
-                                        }
-					url: "https://twitter.com/"+ item.user.screen_name + "/status/" + item.id_str
-                                    };
-                                    counter++;
-                                }
-                            }
-                            return (ret);
-                        }
-                        onmessage = function(event) {
-                            postMessage(process(event.data));
-                        }
-                    });
-                    process.onmessage = function(event) {
-                        var dx = {
-                            statuses: event.data
-                        };
-                        res.send(dx);
-                    };
-                    process.postMessage(obj);
-                    //res.send(data);
-                });
-        };
-
-        exports.tweet = function(req, res) {
-            var idx = req.cookies.idx;
-            var token = req.cookies.token;
-            var secret = req.cookies.tokenSecret;
-            console.log(req.body);
-            console.log("Ewee");
-            oauth.post("https://api.twitter.com/1.1/statuses/update.json", token, secret, {
-                    status: req.body.tweet,
-                    "lat": req.body.latitude,
-                    "long": req.body.longitude,
-                    display_coordinates: true
-                },
-                function() {
-                    res.send("Hello world");
-                });
-        }
-
-
-        exports.search2 = function(req, res) {
-            var queryData = url.parse(req.url, true).query;
-            //res.send(queryData);
-
-            oauth.get('https://api.twitter.com/1.1/search/tweets.json?count=100&geocode=' + queryData.lat + ',' + queryData["long"] + ',1mi',
-                config.keys['accessToken'],
-                config.keys['accessSecret'],
-                function(e, data, resp) {
-                    res.send(data)
-                });
-        }
+    oauth.get('https://api.twitter.com/1.1/search/tweets.json?count=100&geocode=' + queryData.lat + ',' + queryData["long"] + ',1mi',
+        config.keys['accessToken'],
+        config.keys['accessSecret'],
+        function(e, data, resp) {
+            res.send(data)
+        });
+}
